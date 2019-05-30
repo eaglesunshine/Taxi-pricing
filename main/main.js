9ca3e419fb79f7d59d31794d40a2edce783dde94
eaglesunshine <18900927316@163.com>
2019-05-30 09:20:11 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,15 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+	var price=0;
+	var dis=inputs[distance];
+	var pT=inputs[parkTime];
+	
+	if(dis<2)
+		price=Math.round(6+pT*0.25);
+	if(dis>=2 && dis<8)
+		price=Math.round(6+0.8*(dis-2)+pT*0.25);
+	if(dis>=8)
+		price=Math.round(6++0.8*6+0.4*(dis-8)+pT*0.25);
+	
+    return price;
 };
