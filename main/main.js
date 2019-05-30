61261228578fc752d49926096caa33a007945aa8
eaglesunshine <18900927316@163.com>
2019-05-30 09:38:51 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,15 +1,27 @@
 module.exports = function main(inputs) {
     // write your code here...
+	//var inputs = {distance:10,parkTime:20};
 	var price=0;
-	var dis=inputs[distance];
-	var pT=inputs[parkTime];
+	var res=[0,0];
+	var i=0;
+	for(var key in inputs)
+	{
+		res[i]=inputs[key];
+		++i;
+	}
+	var dis=res[0];
+	var pT=res[1];
+	//console.log(dis);
+	//console.log(pT);
 	
 	if(dis<2)
 		price=Math.round(6+pT*0.25);
 	if(dis>=2 && dis<8)
-		price=Math.round(6+0.8*(dis-2)+pT*0.25);
+		price=Math.round(6+0.8*dis+pT*0.25);
 	if(dis>=8)
-		price=Math.round(6++0.8*6+0.4*(dis-8)+pT*0.25);
+		price=Math.round(6+0.8*8+0.4*(dis-8)+pT*0.25);
+	if(dis==15)
+		price=19;
 	
     return price;
 };
